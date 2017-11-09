67fe3519f9d6ef45bc6c808058cfd530b6c177f6
JamesHawking <jakubj91@gmail.com>
2017-11-09 10:46:25 +0100

    XW-4209 | Handle ads in playerStart event

--- src/allow-controll-on-touch.js
+++ src/allow-controll-on-touch.js
@@ -1,24 +1,9 @@
 function wikiaJWPlayerAllowControllOnTouchDevices(elementId, playerInstance) {
-	var adStarted = false;
-
-	playerInstance.on('adStarted', function () {
-		adStarted = true;
-	});
-
-	if (adStarted) {
-		removeMuteButton(playerInstance, elementId, 'adComplete');
-	} else {
-		removeMuteButton(playerInstance, elementId, 'firstFrame');
-	}
-
-}
-
-function removeMuteButton(playerInstance, elementId, event) {
-	playerInstance.on(event, function() {
+	playerInstance.on('playerStart', function () {
 		var unmuteIcon = document.querySelector('.jw-autostart-mute'),
 			jwPlayerNode = document.getElementById(elementId);
 
 		jwPlayerNode.classList.remove('jw-flag-autostart');
 		jwPlayerNode.lastChild.removeChild(unmuteIcon);
-	})
+	});
 }
