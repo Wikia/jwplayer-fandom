57f59db6bbe2f6e81a322d93b7fcb66aa6c84603
JamesHawking <jakubj91@gmail.com>
2017-11-09 12:19:21 +0100

    XW-4209 | Readme update and CR adressed

--- src/allow-controll-on-touch.js
+++ src/allow-controll-on-touch.js
@@ -1,9 +1,9 @@
-function wikiaJWPlayerAllowControllOnTouchDevices(elementId, playerInstance) {
+function wikiaJWPlayerAllowControllOnTouchDevices(playerInstance) {
 	playerInstance.on('playerStart', function () {
 		var unmuteIcon = document.querySelector('.jw-autostart-mute'),
-			jwPlayerClassList = document.getElementById(elementId).classList;
+			jwPlayerClassList = playerInstance.getContainer().classList;
 
 		jwPlayerClassList.remove('jw-flag-autostart');
-		unmuteIcon.style.display = "none";
+		unmuteIcon.style.display = 'none';
 	});
 }
