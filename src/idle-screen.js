4be7225f36b91d392b3454002222c86e38092b59
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-12-05 10:22:24 +0100

    XW-4299 | Adding a missing file

---
+++ src/idle-screen.js
@@ -0,0 +1,34 @@
+function wikiaJWPlayerIdleScreen(playerInstance, i18n) {
+	function showDuration() {
+		var id = playerInstance.id,
+			playerElement = document.getElementById(id),
+			title = playerElement.querySelector('.jw-title'),
+			titlePrimary = playerElement.querySelector('.jw-title-primary'),
+			durationElement = document.createElement('div'),
+			durationWatchElement = document.createElement('span'),
+			durationTimeElement = document.createElement('span');
+
+		durationElement.className = 'wikia-jw-title-duration';
+		durationWatchElement.className = 'wikia-jw-title-duration-watch';
+		durationTimeElement.className = 'wikia-jw-title-duration-time';
+		durationWatchElement.innerText = i18n.watch;
+		window.playerInstance = playerInstance;
+		durationTimeElement.innerText = getUserFriendlyDuration(playerInstance.getDuration());
+
+		durationElement.appendChild(durationWatchElement);
+		durationElement.appendChild(durationTimeElement);
+
+		title.insertBefore(durationElement, titlePrimary);
+	}
+
+	function getUserFriendlyDuration(duration) {
+		var minutes = Math.floor(duration / 60),
+			seconds = duration % 60;
+
+		return minutes + ':' + seconds;
+	}
+
+	playerInstance.on('ready', showDuration);
+}
+
+window.wikiaJWPlayerIdleScreen = wikiaJWPlayerIdleScreen;
