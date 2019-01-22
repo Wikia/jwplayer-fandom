0f648bc6d9c9ba6ed0d058085247d2f960b46775
mszabo-wikia <2721291+mszabo-wikia@users.noreply.github.com>
2019-01-22 12:31:58 +0100

    IW-1231 | Cleanup

--- src/tab-active.js
+++ src/tab-active.js
@@ -2,19 +2,22 @@ function wikiaJWPlayerHandleTabNotActive(playerInstance, willAutoplay) {
 	var pausedOnRelated = false;
 
 	function shouldPlayVideo() {
-		return !document.hidden &&
-			   (['playing', 'paused', 'complete'].indexOf(playerInstance.getState()) === -1 || pausedOnRelated);
+		if (document.hidden) {
+			return false;
+		}
+
+		if (['playing', 'paused', 'complete'].indexOf(playerInstance.getState()) === -1) {
+			return willAutoplay;
+		}
+
+		return pausedOnRelated;
 	}
 
 	function playVideoOnTabSwitch() {
 		if (shouldPlayVideo()) {
-			if (willAutoplay) {
-				playerInstance.play();
-				pausedOnRelated = false;
-				playerInstance.trigger('playerResumedByBrowserTabSwitch');
-			} else {
-				playerInstance.setMute(false);
-			}
+			playerInstance.play();
+			pausedOnRelated = false;
+			playerInstance.trigger('playerResumedByBrowserTabSwitch');
 		}
 	}
 
