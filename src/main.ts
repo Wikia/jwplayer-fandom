3a978779bc9e43f4d4c6b872d0ed04b313255e62
Warren Leaverton <wleaverton@fandom.com>
2022-03-07 12:37:58 -0700

    mobile player

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,6 @@ export * from './players/VideoPlayer';
 
 export { default as DesktopArticleVideoPlayer } from './players/DesktopArticleVideoPlayer/DesktopArticleVideoPlayer';
 export * from './players/DesktopArticleVideoPlayer/DesktopArticleVideoPlayer';
+
+export { default as MobileArticleVideoPlayer } from './players/MobileArticleVideoPlayer/MobileArticleVideoPlayer';
+export * from './players/MobileArticleVideoPlayer/MobileArticleVideoPlayer';
---
+++ src/players/MobileArticleVideoPlayer/MobileArticleVideoPlayer.tsx
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react';
+import styled, { css } from 'styled-components';
+import JwPlayerWrapper from 'players/shared/JwPlayerWrapper';
+import useOnScreen from 'utils/useOnScreen';
+import useAdComplete from 'utils/useAdComplete';
+import PlayerWrapper from 'players/shared/PlayerWrapper';
+
+const MobileArticleVideoTopPlaceholder = styled.div`
+	background-color: black;
+	width: 100%;
+	height: 100%;
+`;
+
+interface MobileArticleVideoWrapperProps {
+	visibleOnScreen: boolean;
+}
+
+const MobileArticleVideoWrapper = styled.div<MobileArticleVideoWrapperProps>`
+	${(props) =>
+		!props.visibleOnScreen &&
+		css`
+			box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+			position: fixed;
+			top: 55px;
+			width: 100%;
+		`}
+`;
+
+const MobileArticleVideoPlayer: React.FC = () => {
+	const ref = useRef<HTMLDivElement>(null);
+	const adComplete = useAdComplete();
+	const onScreen = useOnScreen(ref);
+
+	return (
+		<PlayerWrapper>
+			<MobileArticleVideoTopPlaceholder ref={ref}>
+				{adComplete && (
+					<MobileArticleVideoWrapper visibleOnScreen={onScreen}>
+						<JwPlayerWrapper />
+					</MobileArticleVideoWrapper>
+				)}
+			</MobileArticleVideoTopPlaceholder>
+		</PlayerWrapper>
+	);
+};
+
+export default MobileArticleVideoPlayer;
