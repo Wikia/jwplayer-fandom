be17b7eb2b3c316dcae3bf8ba891f95afeacdae3
Warren Leaverton <wleaverton@fandom.com>
2022-03-21 13:52:54 -0700

    fixing generic player

--- src/players/VideoPlayer.tsx
+++ src/players/VideoPlayer.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { PlaylistItem } from 'types';
+
+import PlayerWrapper from 'players/shared/PlayerWrapper';
 
 import JwPlayerWrapper from './shared/JwPlayerWrapper';
 
-const VideoPlayer: React.FC = () => <JwPlayerWrapper />;
+interface VideoPlayerProps {
+	playlist: string | PlaylistItem[];
+}
+
+const VideoPlayer: React.FC<VideoPlayerProps> = ({ playlist }) => {
+	const ref = useRef<HTMLDivElement>(null);
+
+	return (
+		<PlayerWrapper>
+			<div ref={ref}>
+				<JwPlayerWrapper playlist={playlist} />;
+			</div>
+		</PlayerWrapper>
+	);
+};
 
 export default VideoPlayer;
