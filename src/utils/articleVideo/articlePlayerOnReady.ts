def824982bb760cdfaabb041a55b12efa6d0af7d
Warren Leaverton <wleaverton@fandom.com>
2022-05-09 16:49:53 -0700

    DPAD-528: removing bad api call

--- src/utils/articleVideo/articlePlayerOnReady.ts
+++ src/utils/articleVideo/articlePlayerOnReady.ts
@@ -3,39 +3,36 @@ import { setVideoSeenInSession } from 'utils/articleVideo/articleVideoSession';
 import { willAutoplay, willMute } from 'utils/articleVideo/articleVideoConfig';
 
 export default function useOnArticlePlayerReady(videoDetails, playerInstance): void {
-	const player = playerInstance;
 	const playerKey = 'aeJWPlayerKey';
 
-	player?.on('ready', () => {
-		if (!videoDetails.isDedicatedForArticle) {
-			setVideoSeenInSession();
-		}
+	if (!videoDetails.isDedicatedForArticle) {
+		setVideoSeenInSession();
+	}
 
-		window.dispatchEvent(new CustomEvent('wikia.jwplayer.instanceReady', { detail: player }));
-		window[playerKey] = player;
+	window.dispatchEvent(new CustomEvent('wikia.jwplayer.instanceReady', { detail: playerInstance }));
+	window[playerKey] = playerInstance;
 
-		communicationService.dispatch({
-			type: '[JWPlayer] Player Ready',
-			playerKey,
-			targeting: {
-				plist: videoDetails.feedid || '',
-				vtags: videoDetails.videoTags || '',
-				videoScope: videoDetails.isDedicatedForArticle ? 'article' : 'wiki',
-			},
-			options: {
-				audio: !willMute(),
-				ctp: !willAutoplay(),
-				slotName: 'featured',
-				videoId: videoDetails.playlist[0].mediaid,
-			},
-		});
-
-		// player.on('autoplayToggle', function (data) {
-		//     articleVideoCookieService.setAutoplay(data.enabled ? '1' : '0');
-		// });
-
-		// player.on('captionsSelected', function (data) {
-		//     articleVideoCookieService.setCaptions(data.selectedLang);
-		// });
+	communicationService.dispatch({
+		type: '[JWPlayer] Player Ready',
+		playerKey,
+		targeting: {
+			plist: videoDetails.feedid || '',
+			vtags: videoDetails.videoTags || '',
+			videoScope: videoDetails.isDedicatedForArticle ? 'article' : 'wiki',
+		},
+		options: {
+			audio: !willMute(),
+			ctp: !willAutoplay(),
+			slotName: 'featured',
+			videoId: videoDetails.playlist[0].mediaid,
+		},
 	});
+
+	// player.on('autoplayToggle', function (data) {
+	//     articleVideoCookieService.setAutoplay(data.enabled ? '1' : '0');
+	// });
+
+	// player.on('captionsSelected', function (data) {
+	//     articleVideoCookieService.setCaptions(data.selectedLang);
+	// });
 }
