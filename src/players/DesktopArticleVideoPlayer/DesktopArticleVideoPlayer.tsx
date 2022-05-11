85ae359de63544592b8034c4ded07b7314af156a
Warren Leaverton <wleaverton@fandom.com>
2022-05-11 14:50:23 -0700

    dpad-528: hiding icons on small player

--- src/players/DesktopArticleVideoPlayer/DesktopArticleVideoPlayer.tsx
+++ src/players/DesktopArticleVideoPlayer/DesktopArticleVideoPlayer.tsx
@@ -83,11 +83,20 @@ const DesktopArticleVideoPlayer: React.FC<DesktopArticleVideoPlayerProps> = ({ v
 	const right = boundingClientRect?.right;
 	const width = boundingClientRect?.width;
 	const controlbar = document.querySelector<HTMLElement>('.jw-controlbar');
+	const shareIcon = document.querySelector<HTMLElement>('.jw-controlbar .jw-button-container .jw-settings-sharing');
+	const moreVideosIcon = document.querySelector<HTMLElement>('.jw-controlbar .jw-button-container .jw-related-btn');
+	const pipIcon = document.querySelector<HTMLElement>('.jw-controlbar .jw-button-container .jw-icon-pip');
 
 	if (onScreen) {
 		if (controlbar) controlbar.style.background = 'rgba(0, 0, 0, 0.5)';
+		if (shareIcon) shareIcon.style.display = 'flex';
+		if (moreVideosIcon) moreVideosIcon.style.display = 'flex';
+		if (pipIcon) pipIcon.style.display = 'flex';
 	} else {
 		if (controlbar) controlbar.style.background = 'linear-gradient(0,#000,transparent)';
+		if (shareIcon) shareIcon.style.display = 'none';
+		if (moreVideosIcon) moreVideosIcon.style.display = 'none';
+		if (pipIcon) pipIcon.style.display = 'none';
 	}
 
 	return (
