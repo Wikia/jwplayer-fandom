c68a5cfda72bf26d57b663478bd11c64ecc6ef17
Warren Leaverton <wleaverton@fandom.com>
2022-08-08 13:50:08 -0700

    DPAD-955: removing twitch from player wrapper

--- src/loaders/DesktopArticleVideoLoader.tsx
+++ src/loaders/DesktopArticleVideoLoader.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import JWDesktopArticleVideoPlayer from 'jwplayer/players/DesktopArticleVideoPlayer/DesktopArticleVideoPlayer';
-import TwitchDesktopArticleVideoPlayer from 'twitch/players/DesktopArticleVideoPlayer';
 import { DesktopArticleVideoLoaderProps } from 'loaders/types';
-import checkTwitchTakeover from 'loaders/utils/CheckTwitchTakeover';
 
 const DesktopArticleVideoLoader: React.FC<DesktopArticleVideoLoaderProps> = ({ videoDetails }) => {
-	const twitchTakeover = checkTwitchTakeover();
-
-	if (twitchTakeover) {
-		return <TwitchDesktopArticleVideoPlayer />;
-	}
-
 	// Default to JW Player
 	return <JWDesktopArticleVideoPlayer videoDetails={videoDetails} />;
 };
--- src/loaders/MobileArticleVideoLoader.tsx
+++ src/loaders/MobileArticleVideoLoader.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import JWMobileArticleVideoPlayer from 'jwplayer/players/MobileArticleVideoPlayer/MobileArticleVideoPlayer';
-import TwitchMobileArticleVideoPlayer from 'twitch/players/MobileArticleVideoPlayer';
 import { MobileArticleVideoLoaderProps } from 'loaders/types';
-import checkTwitchTakeover from 'loaders/utils/CheckTwitchTakeover';
 
 const MobileArticleVideoLoader: React.FC<MobileArticleVideoLoaderProps> = ({
 	hasPartnerSlot,
 	isFullScreen,
 	videoDetails,
 }) => {
-	const twitchTakeover = checkTwitchTakeover();
-
-	if (twitchTakeover) {
-		return <TwitchMobileArticleVideoPlayer />;
-	}
-
 	// Default to JW Player
 	return (
 		<JWMobileArticleVideoPlayer
