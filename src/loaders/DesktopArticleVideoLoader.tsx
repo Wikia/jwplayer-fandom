39854ce6bba06ab49388ee6abd4a40a9482753ef
Ivan Alburquerque <ialburquerque@fandom.com>
2022-08-30 17:22:36 -0400

    DPAD-816 :: Clean old code

--- src/loaders/DesktopArticleVideoLoader.tsx
+++ src/loaders/DesktopArticleVideoLoader.tsx
@@ -1,27 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { DesktopArticleVideoLoaderProps } from 'loaders/types';
 import { setVersionWindowVar } from 'loaders/utils/GetVersion';
-import { checkIfUserInNoVideoExperiment } from 'utils/experiments/experiments';
-import { jwPlayerExperimentTracker } from 'jwplayer/utils/videoTracking';
-import { ExperimentWindowInterface } from 'utils/experiments/bucketingUtils';
 
 export { getVideoPlayerVersion } from 'loaders/utils/GetVersion';
-
-declare let window: ExperimentWindowInterface;
 
 export const DesktopArticleVideoLoader: React.FC<DesktopArticleVideoLoaderProps> = ({ videoDetails }) => {
 	const [player, setPlayer] = useState(undefined);
 
 	useEffect(() => {
 		if (!player) {
-			console.log('checking if user is in experiment');
-			if (checkIfUserInNoVideoExperiment()) {
-				console.log('user is in experiment, disabling player');
-				window.__user_in_no_video_experiment = true;
-				jwPlayerExperimentTracker.singleTrack('desktop-no-video-experiment');
-			} else {
-				getPlayer();
-			}
+			getPlayer();
 		}
 		setVersionWindowVar();
 	}, []);
