4005cf31aae6e9d99a05560c4b4485383ff46b36
Warren Leaverton <wleaverton@fandom.com>
2022-09-08 13:03:19 -0700

    DPAD-995: removing reskinned loader reference

--- test-jw/src/App.js
+++ test-jw/src/App.js
@@ -1,5 +1,5 @@
 import CanonicalVideoLoader from '@fandom/jwplayer-fandom/CanonicalVideoLoader'
-import { DesktopReskinnedArticleVideoLoader } from '@fandom/jwplayer-fandom/DesktopReskinnedArticleVideoLoader';
+import { DesktopArticleVideoLoader } from '@fandom/jwplayer-fandom/DesktopArticleVideoLoader';
 import { WIREWAX_VIDEO, CANONICAL_VIDEO, ARTICLE_VIDEO_DETAILS } from './videoConfigs';
 import './app.css';
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       {/*<CanonicalVideoLoader currentVideo={CANONICAL_VIDEO} />*/}
-		  <DesktopReskinnedArticleVideoLoader videoDetails={ARTICLE_VIDEO_DETAILS} />
+		  <DesktopArticleVideoLoader videoDetails={ARTICLE_VIDEO_DETAILS} />
     </div>
   );
 }
