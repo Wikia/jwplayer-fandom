c849c3633214bcdf1f9c13a426d9dbe47380ca3e
Warren Leaverton <wleaverton@fandom.com>
2022-09-15 14:13:28 -0700

    turning off and backing up scroll player

--- src/jwplayer/players/DesktopArticleVideoPlayer/DesktopArticleVideoScrollPlayer.tsx
+++ src/jwplayer/players/DesktopArticleVideoPlayer/DesktopArticleVideoScrollPlayer.tsx
@@ -1,10 +1,14 @@
-import React, { useRef } from 'react';
-import styled from 'styled-components';
+import React, { useRef, useState } from 'react';
+import WDSVariables from '@fandom-frontend/design-system/dist/variables.json';
+import styled, { css, keyframes } from 'styled-components';
 import UnmuteButton from 'jwplayer/players/DesktopArticleVideoPlayer/UnmuteButton';
 import JwPlayerWrapper from 'jwplayer/players/shared/JwPlayerWrapper';
+import VideoDetails from 'jwplayer/players/DesktopArticleVideoPlayer/VideoDetails';
+import useOnScreen from 'utils/useOnScreen';
 import useAdComplete from 'jwplayer/utils/useAdComplete';
 import PlayerWrapper from 'jwplayer/players/shared/PlayerWrapper';
 import { DesktopArticleVideoPlayerProps } from 'jwplayer/types';
+import CloseButton from 'jwplayer/players/shared/CloseButton';
 import Attribution from 'jwplayer/players/DesktopArticleVideoPlayer/Attribution';
 import { getArticleVideoConfig } from 'jwplayer/utils/articleVideo/articleVideoConfig';
 import articlePlayerOnReady from 'jwplayer/utils/articleVideo/articlePlayerOnReady';
@@ -20,20 +24,44 @@ const DesktopArticleVideoTopPlaceholder = styled.div`
 	z-index: 2;
 `;
 
+const moveDownAnimation = (right: number, width: number) => keyframes`
+	from {
+		right: ${right}px;
+		bottom: 100%;
+		width: ${width}px;
+	}
+
+	to {
+		right: 18px;
+		bottom: 45px;
+		width: 300px;  
+	}
+`;
+
 interface DesktopArticleVideoWrapperProps {
+	isScrollPlayer: boolean;
 	right?: number;
 	width?: number;
 }
 
 const DesktopArticleVideoWrapper = styled.div<DesktopArticleVideoWrapperProps>`
 	height: max-content;
-	position: absolute;
-	bottom: 0;
-	right: 0;
-	top: 0;
-	left: 0;
-	transform: translateZ(0);
-	-webkit-transform: translateZ(0);
+	${(props) =>
+		props.isScrollPlayer
+			? css`
+					z-index: ${Number(WDSVariables.z2) + 2};
+					position: fixed;
+					animation: ${moveDownAnimation(props.right, props.width)} 0.4s normal forwards;
+			  `
+			: css`
+					position: absolute;
+					bottom: 0;
+					right: 0;
+					top: 0;
+					left: 0;
+					transform: translateZ(0);
+					-webkit-transform: translateZ(0);
+			  `}
 `;
 
 const TopBar = styled.div`
@@ -44,6 +72,9 @@ const TopBar = styled.div`
 const DesktopArticleVideoPlayer: React.FC<DesktopArticleVideoPlayerProps> = ({ videoDetails }) => {
 	const placeholderRef = useRef<HTMLDivElement>(null);
 	const adComplete = useAdComplete();
+	const onScreen = useOnScreen(placeholderRef, '0px', 0.1);
+	const [dismissed, setDismissed] = useState(false);
+	const isScrollPlayer = !(dismissed || onScreen);
 	const boundingClientRect = placeholderRef.current?.getBoundingClientRect();
 	const right = boundingClientRect?.right;
 	const width = boundingClientRect?.width;
@@ -52,23 +83,37 @@ const DesktopArticleVideoPlayer: React.FC<DesktopArticleVideoPlayerProps> = ({ v
 	const moreVideosIcon = document.querySelector<HTMLElement>('.jw-controlbar .jw-button-container .jw-related-btn');
 	const pipIcon = document.querySelector<HTMLElement>('.jw-controlbar .jw-button-container .jw-icon-pip');
 
-	if (controlbar) controlbar.style.background = 'linear-gradient(0,#000,transparent)';
-	if (shareIcon) shareIcon.style.display = 'none';
-	if (moreVideosIcon) moreVideosIcon.style.display = 'none';
-	if (pipIcon) pipIcon.style.display = 'none';
+	if (onScreen) {
+		if (controlbar) controlbar.style.background = 'rgba(0, 0, 0, 0.5)';
+		if (shareIcon) shareIcon.style.display = 'flex';
+		if (moreVideosIcon) moreVideosIcon.style.display = 'flex';
+		if (pipIcon) pipIcon.style.display = 'flex';
+	} else {
+		if (controlbar) controlbar.style.background = 'linear-gradient(0,#000,transparent)';
+		if (shareIcon) shareIcon.style.display = 'none';
+		if (moreVideosIcon) moreVideosIcon.style.display = 'none';
+		if (pipIcon) pipIcon.style.display = 'none';
+	}
 
 	return (
 		<PlayerWrapper playerName="jw-desktop-article-video">
 			<DesktopArticleVideoTopPlaceholder ref={placeholderRef}>
 				{adComplete && (
-					<DesktopArticleVideoWrapper className={'desktop-article-video-wrapper'} right={right} width={width}>
+					<DesktopArticleVideoWrapper
+						className={'desktop-article-video-wrapper'}
+						right={right}
+						width={width}
+						isScrollPlayer={isScrollPlayer}
+					>
 						<TopBar>
-							<UnmuteButton />
+							{!isScrollPlayer && <UnmuteButton />}
+							{isScrollPlayer && <CloseButton dismiss={() => setDismissed(true)} />}
 						</TopBar>
 						<JwPlayerWrapper
 							config={getArticleVideoConfig(videoDetails)}
 							onReady={(playerInstance) => articlePlayerOnReady(videoDetails, playerInstance)}
 						/>
+						{isScrollPlayer && <VideoDetails />}
 					</DesktopArticleVideoWrapper>
 				)}
 			</DesktopArticleVideoTopPlaceholder>
