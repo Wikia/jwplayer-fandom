14d9b96d9bbbd990388df812850df7e24b209f79
Warren Leaverton <wleaverton@fandom.com>
2022-09-30 14:57:42 -0700

    DPAD-995: formatting time

--- src/experimental/shared/TimeRemaining.tsx
+++ src/experimental/shared/TimeRemaining.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 import useTime from 'jwplayer/utils/useTime';
 
-const TimeRemainingWrapper = styled.div``;
+const TimeRemainingWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	color: #fff;
+`;
 
 const TimeRemaining: React.FC = () => {
 	const time = useTime();
 
+	const formatTime = (time) => {
+		if (time < 3600) {
+			return new Date(time * 1000).toISOString().substring(14, 19);
+		} else {
+			return new Date(time * 1000).toISOString().substring(11, 16);
+		}
+	};
+
 	return (
 		<TimeRemainingWrapper>
-			{time.position}/{time.duration}
+			{formatTime(time.position)} / {formatTime(time.duration)}
 		</TimeRemainingWrapper>
 	);
 };
