b77807050b749d966bece2b0d801aac83f0136bb
pawelforfa <pforfa@fandom.com>
2022-10-05 17:10:22 -0400

    DPAD-1189 :: Testing jw package version with query param so that QA can reproduce the bandwidth issue that occurred in the jwplayer and UCP; the player will ignore the results of the canPlayVideo() function, and always return true, causing the player to show up in a hidden ucp video player container, and playing indefinitely

--- src/loaders/utils/shouldLoadPlayer.ts
+++ src/loaders/utils/shouldLoadPlayer.ts
@@ -1,10 +1,20 @@
-interface WindowWithCanPlayVideo {
+interface WindowWithCanPlayVideo extends Window {
 	canPlayVideo: () => boolean;
 }
 
 declare let window: WindowWithCanPlayVideo;
 
 export const shouldLoadUcpPlayer = () => {
+	if (typeof window === 'undefined') {
+		return false;
+	}
+	if (window.location.search.indexOf('jw_always_display=true') > -1) {
+		console.debug(
+			'jw_always_display=true was set. Player will always show up, even if the canPlayVideo() function evaluates to false.',
+		);
+		return true;
+	}
+
 	const isPlayVideoAFunction = typeof window?.canPlayVideo === 'function';
 	console.debug('isPlayVideoAFunction: ', isPlayVideoAFunction);
 
