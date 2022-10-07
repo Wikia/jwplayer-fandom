e9d203862d00b3fa2674bd22f9c9c4a28972412b
pawelforfa <pforfa@fandom.com>
2022-10-07 15:02:25 -0400

    DPAD-1124 :: Changed logic to ensure player can load on localhost.

--- src/loaders/utils/shouldLoadPlayer.ts
+++ src/loaders/utils/shouldLoadPlayer.ts
@@ -7,15 +7,17 @@ interface WindowWithCanPlayVideo {
 declare let window: WindowWithCanPlayVideo;
 
 export const shouldLoadUcpPlayer = () => {
+	const onLocalhost = isLocalhost();
+	if (onLocalhost) {
+		console.debug('On localhost. Player will load.');
+		return true;
+	}
+
 	const isPlayVideoAFunction = typeof window?.canPlayVideo === 'function';
 	console.debug('isPlayVideoAFunction: ', isPlayVideoAFunction);
 
 	const canPlay = isPlayVideoAFunction && window?.canPlayVideo();
 	console.debug('Can Play Video on UCP? ', canPlay);
-	console.debug('Can play Video on UCP with localhost: ', isLocalhost());
 
-	// TODO: Check if this should be removed
-	const finalResult = canPlay || isLocalhost();
-	console.debug('Can play Video final result: ', finalResult);
-	return finalResult;
+	return canPlay;
 };
