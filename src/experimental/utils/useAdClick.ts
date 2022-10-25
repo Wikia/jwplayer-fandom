93e5690d73c6a4015d661251135e5815f63f3561
Warren Leaverton <wleaverton@fandom.com>
2022-10-25 14:53:49 -0700

    DPAD-995: removing unused file

--- src/experimental/utils/useAdClick.ts
+++
@@ -1,6 +0,0 @@
-export const useAdClick = () => {
-	const clickEvent = new MouseEvent('click');
-
-	console.log('click da video', clickEvent);
-	document.getElementById('featured-video__player').dispatchEvent(clickEvent);
-};
