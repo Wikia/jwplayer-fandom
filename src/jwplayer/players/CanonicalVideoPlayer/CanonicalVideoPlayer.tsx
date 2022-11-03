445097a78582785d352b895c15f55760207aa99b
Ivan Alburquerque <ialburquerque@fandom.com>
2022-11-03 15:52:42 -0400

    DPAD-1140 :: Forgot main changes🤦‍♂️

--- src/jwplayer/players/CanonicalVideoPlayer/CanonicalVideoPlayer.tsx
+++ src/jwplayer/players/CanonicalVideoPlayer/CanonicalVideoPlayer.tsx
@@ -3,7 +3,6 @@ import { CanonicalVideoPlayerProps } from 'jwplayer/types';
 import PlayerWrapper from 'jwplayer/players/shared/PlayerWrapper';
 import LoadableVideoPlayerWrapper from 'jwplayer/players/shared/LoadableVideoPlayerWrapper';
 import styled, { css } from 'styled-components';
-import useOnScreen from 'utils/useOnScreen';
 import useAdComplete from 'jwplayer/utils/useAdComplete';
 import { communicationService } from 'jwplayer/utils/communication';
 import { isLocalDevelopment, isOnBrowser } from 'jwplayer/utils/envs';
@@ -41,8 +40,6 @@ const CanonicalVideoWrapper = styled.div<CanonicalVideoWrapperProps>`
 const CanonicalVideoPlayer: React.FC<CanonicalVideoPlayerProps> = ({ currentVideo, videoDetails, onComplete }) => {
 	const ref = useRef<HTMLDivElement>(null);
 	const adComplete = useAdComplete();
-	const onScreen = useOnScreen(ref, '0px', 1);
-	const isScrollPlayer = !onScreen;
 
 	useEffect(() => {
 		const payload = {
@@ -58,7 +55,7 @@ const CanonicalVideoPlayer: React.FC<CanonicalVideoPlayerProps> = ({ currentVide
 		<PlayerWrapper playerName="canonical-video-player">
 			<CanonicalVideoTopPlaceholder ref={ref}>
 				{adComplete && (
-					<CanonicalVideoWrapper isScrollPlayer={isScrollPlayer}>
+					<CanonicalVideoWrapper isScrollPlayer={false}>
 						<LoadableVideoPlayerWrapper
 							currentVideo={currentVideo}
 							videoDetails={videoDetails}
