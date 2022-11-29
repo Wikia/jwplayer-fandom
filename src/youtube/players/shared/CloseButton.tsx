8f37e38ad0a6415d4b125ea9fe0729edc7a8aada
pawelforfa <pforfa@fandom.com>
2022-11-29 17:50:42 -0500

    DPAD-1089 :: Changed the width/height and right/top params of the youtube close button so that it doesn't overlap in the mini player with the youtube player's share overlay's close button | Bump test package version

--- src/youtube/players/shared/CloseButton.tsx
+++ src/youtube/players/shared/CloseButton.tsx
@@ -7,18 +7,19 @@ import {
 	YoutubePlayerTrackingProps,
 } from 'youtube/players/shared/youtubeTrackingEvents';
 
-const CloseWrapper = styled.div<{ topOffset?: number }>`
+const CloseWrapper = styled.div<YoutubePlayerTrackingProps>`
 	cursor: pointer;
 	pointer-events: initial;
-	height: 36px;
-	width: 36px;
+	height: ${(props) => (props.deviceType === 'desktop' ? '24px' : '36px')};
+	width: ${(props) => (props.deviceType === 'desktop' ? '24px' : '36px')};
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	opacity: 0.98;
 	position: absolute;
-	right: 0;
-	top: ${(props) => (props.topOffset ? `${props.topOffset}px` : 0)};
+	right: -3px;
+	right: ${(props) => (props.deviceType === 'desktop' ? '-3px' : '-6px')};
+	top: -3px;
 	z-index: ${Number(WDSVariables.z7) + 1};
 `;
 
@@ -28,17 +29,16 @@ const CrossIcon = styled(IconCrossTiny)`
 
 interface CloseButtonProps extends YoutubePlayerTrackingProps {
 	dismiss: () => void;
-	topOffset?: number;
 }
 
-const CloseButton: React.FC<CloseButtonProps> = ({ dismiss, topOffset, deviceType }) => {
+const CloseButton: React.FC<CloseButtonProps> = ({ dismiss, deviceType }) => {
 	const onClickClose = () => {
 		trackYoutubePlayerScrollClose({ deviceType: deviceType });
 		dismiss();
 	};
 
 	return (
-		<CloseWrapper topOffset={topOffset} onClick={onClickClose}>
+		<CloseWrapper deviceType={deviceType} onClick={onClickClose}>
 			<CrossIcon />
 		</CloseWrapper>
 	);
