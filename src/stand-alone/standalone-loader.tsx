1b1c2a861cd09a929ef2e1ed2c441ca1e46afa1c
pawelforfa <pforfa@fandom.com>
2023-01-05 18:40:51 -0500

    DPAD-1366 :: Moved a majority of the helper functions in the standalone-loader.tsx file into a new loaderHelper.ts file to keep the former file clean | Created a new RequireOnlyOne type, which allows an interface to choose between multiple optional properties and requiring one and only one of them to be set | Annotated the RedVenturePlayerContext interface with @description and @example tags | Moved all the context prop checks into a separate canPlayerRender function, which takes care of all the prop checks and return true/false | the loadPlayer function now throws an error when the canPlayerRender function returns false - this is useful to creating a catch on the Promise that this function returns in the function caller, and allows additional action for logging | Created a getVideoWrapperElement function which determines how the videoWrapperElement will be generated (pregenerated or found with document.querySelector) based on the embedSelector and embedHtmlElement options

--- src/stand-alone/standalone-loader.tsx
+++ src/stand-alone/standalone-loader.tsx
@@ -2,54 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { RedVentureVideoDetails, RedVentureVideoPlayerProps } from 'jwplayer/types';
 import RedVentureVideoPlayer from 'jwplayer/players/RedVentureVideoPlayer/RedVentureVideoPlayer';
+import {
+	buildRedVentureVideoDetails,
+	canPlayerRender,
+	getVideoDetails,
+	getVideoWrapperElement,
+} from 'stand-alone/loaderHelper';
+import { RedVenturePlayerContextProps } from 'stand-alone/loaderHelper';
 
 console.debug('Standalone RedVenture Video Player ...');
-
-interface RedVenturePlayerContextProps {
-	contextName: string;
-	embedSelector: string;
-	mediaId?: string;
-	showScrollPlayer?: boolean;
-}
 
 interface WindowWithRedVentureJWPlayer extends Window {
 	loadPlayer: (props: RedVenturePlayerContextProps) => void;
 }
 
 declare let window: WindowWithRedVentureJWPlayer;
-
-async function getVideoDetails(mediaId: string) {
-	try {
-		const response = await fetch('https://cdn.jwplayer.com/v2/media' + '/' + mediaId, {
-			method: 'get',
-		});
-
-		if (!response.ok) {
-			console.error(`Could not find video details for mediaId: ${mediaId}`);
-			return null;
-		}
-
-		return await response.json();
-	} catch (e) {
-		console.error(`Could not fetch the video details for mediaId: ${mediaId}`, e);
-	}
-}
-
-const buildRedVentureVideoDetails = (jwDetails): RedVentureVideoDetails => {
-	if (!jwDetails) {
-		return null;
-	}
-	return {
-		title: jwDetails?.title,
-		description: jwDetails?.description,
-		kind: jwDetails?.kind,
-		playlist: jwDetails?.playlist,
-		feed_instance_id: jwDetails?.feed_instance_id,
-		videoTags: jwDetails?.playlist?.[0]?.tags,
-		duration: jwDetails?.playlist?.[0]?.duration,
-		mediaId: jwDetails?.playlist?.[0]?.mediaId,
-	};
-};
 
 // TODO: Investigate the following
 /*
@@ -67,31 +34,18 @@ const buildRedVentureVideoDetails = (jwDetails): RedVentureVideoDetails => {
  * */
 
 window.loadPlayer = async (context: RedVenturePlayerContextProps) => {
-	if (!context) {
-		console.error('A context object was not provided for the RedVenture JW Player.');
-		return;
+	const canProceed = canPlayerRender(context);
+
+	if (!canProceed) {
+		throw new Error('Could not render the video player. The requirements for initialization were not met.');
 	}
-	if (!context?.contextName) {
-		console.error('A contextName has to be provided in the context object.');
-		return;
-	}
-	if (!context?.embedSelector) {
-		console.error(
-			'An embed element selector must be provided in the context object, so that the video player can render on the page.',
-		);
-	}
-	if (!context?.mediaId) {
-		console.error('A mediaId has to be provided in the context object.');
-	}
-	console.debug('context object: ', context);
-	console.debug('Will show mini player: ', context?.showScrollPlayer ?? false);
 
 	const jwMediaDetails = await getVideoDetails(context.mediaId);
 	const redVentureVideoDetails: RedVentureVideoDetails = buildRedVentureVideoDetails(jwMediaDetails);
 
 	const reactRoot = document.createElement('div');
-	const videoWrapperEl = document.querySelector(context?.embedSelector);
-	videoWrapperEl.innerHTML = '';
+	const videoWrapperElement = getVideoWrapperElement(context);
+	videoWrapperElement.innerHTML = '';
 
 	console.debug('Loading in the RedVentureVideoPlayer in the standalone-loader');
 	ReactDOM.render(
@@ -101,5 +55,5 @@ window.loadPlayer = async (context: RedVenturePlayerContextProps) => {
 		} as RedVentureVideoPlayerProps),
 		reactRoot,
 	);
-	videoWrapperEl.appendChild(reactRoot);
+	videoWrapperElement.appendChild(reactRoot);
 };
