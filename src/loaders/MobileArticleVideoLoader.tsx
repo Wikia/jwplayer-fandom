88d11357890bf649106f44e4a1918e36fd2e657f
Warren Leaverton <wleaverton@fandom.com>
2023-01-10 15:39:42 -0700

    DPAD-996: fixing mobile loader

--- src/loaders/MobileArticleVideoLoader.tsx
+++ src/loaders/MobileArticleVideoLoader.tsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { MobileArticleVideoLoaderProps } from 'loaders/types';
 import { setVersionWindowVar } from 'loaders/utils/GetVersion';
+import defineExperiment from '@fandom/pathfinder-lite/experiments/defineExperiment';
+import getExperiment from '@fandom/pathfinder-lite/experiments/getExperiment';
+import { Experiment } from '@fandom/pathfinder-lite/types';
 import { shouldLoadUcpPlayer } from 'loaders/utils/shouldLoadPlayer';
 
 export { getVideoPlayerVersion } from 'loaders/utils/GetVersion';
 
-export const MobileArticleVideoLoader: React.FC<MobileArticleVideoLoaderProps> = ({
-	hasPartnerSlot,
-	isFullScreen,
-	videoDetails,
-}) => {
+const mobileReskinnedExperiment = defineExperiment({
+	name: 'mobile-reskinned-player',
+	// TODO: figure out bucket for release
+	buckets: ['p'],
+	// TODO: Change this before MERGING!! Used for testing purposes.
+	// TODO: figure out date to release
+	startDate: Date.parse('2023-01-09T08:00:00'),
+	endDate: Date.parse('2023-01-20T11:59:00'),
+});
+
+export const MobileArticleVideoLoader: React.FC<MobileArticleVideoLoaderProps> = ({ videoDetails }) => {
 	const [player, setPlayer] = useState(undefined);
 
 	useEffect(() => {
@@ -21,17 +30,25 @@ export const MobileArticleVideoLoader: React.FC<MobileArticleVideoLoaderProps> =
 	}, []);
 
 	const getPlayer = async () => {
-		// By default set the base player
-		import('jwplayer/players/MobileArticleVideoPlayer/MobileArticleVideoPlayer').then(
-			({ default: JWMobileArticleVideoPlayer }) =>
-				setPlayer(
-					<JWMobileArticleVideoPlayer
-						hasPartnerSlot={hasPartnerSlot}
-						videoDetails={videoDetails}
-						isFullScreen={isFullScreen}
-					/>,
-				),
-		);
+		const currentExperiment: Experiment = getExperiment([mobileReskinnedExperiment]);
+
+		// By default if there is no experiment just set the base player
+		if (!currentExperiment) {
+			import('jwplayer/players/MobileArticleVideoPlayer/MobileArticleVideoPlayer').then(
+				({ default: JWMobileArticleVideoPlayer }) =>
+					setPlayer(<JWMobileArticleVideoPlayer videoDetails={videoDetails} />),
+			);
+
+			return;
+		}
+
+		if (currentExperiment?.name === mobileReskinnedExperiment?.name) {
+			currentExperiment.log.info('Loading re-skinned Mobile Article Video Player');
+			import('experimental/players/MobileReskinnedArticleVideoPlayer/MobileReskinnedArticleVideoPlayer').then(
+				({ default: JWMobileReskinnedArticleVideoPlayer }) =>
+					setPlayer(<JWMobileReskinnedArticleVideoPlayer videoDetails={videoDetails} />),
+			);
+		}
 	};
 
 	return player;
