29cb9b3111464cfb1433c968177677b5bfd29b64
pawelforfa <pforfa@fandom.com>
2023-01-13 12:14:48 -0500

    DPAD-1371 :: Increment jwPlayerKeyCounter after the RV player has been instantiated, so that the next time this player gets created, a new key will be created on the window object that'll reference the newly created player, instead of always having a reference point to the first one that was created

--- src/jwplayer/players/RedVentureVideoPlayer/redVenturePlayerOnReady.ts
+++ src/jwplayer/players/RedVentureVideoPlayer/redVenturePlayerOnReady.ts
@@ -2,10 +2,11 @@ import { communicationService } from 'jwplayer/utils/communication';
 import { recordVideoEvent, VIDEO_RECORD_EVENTS } from 'jwplayer/utils/videoTimingEvents';
 import { RedVentureVideoDetails } from 'jwplayer/types';
 
-const jwPlayerKeyCounter = 0;
+let jwPlayerKeyCounter = 0;
 
 export default function useOnRedVenturePlayerReady(videoDetails: RedVentureVideoDetails, playerInstance): void {
 	const playerKey = 'aeJWPlayerKey' + (jwPlayerKeyCounter === 0 ? '' : jwPlayerKeyCounter);
+	jwPlayerKeyCounter++;
 
 	window.dispatchEvent(new CustomEvent('wikia.jwplayer.instanceReady', { detail: playerInstance }));
 	window[playerKey] = playerInstance;
