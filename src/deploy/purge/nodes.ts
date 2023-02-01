ecf625ff1f3c91c81d59952b75d424da26b3da96
pawelforfa <pforfa@fandom.com>
2023-02-01 17:00:12 -0500

    DPAD-1273 :: change the nodes.ts list for the purgeSingleFile script

--- src/deploy/purge/nodes.ts
+++ src/deploy/purge/nodes.ts
@@ -1,3 +1,4 @@
+// knife search roles:cdn-node* -a hostname | grep hostname | awk '{print $2}' | sort | sed "s/\(.*\)/\t'\1',/g"
 export const NODES: Array<string> = [
 	'dev-wk-cdn-s1',
 	'dev-wk-cdn-s2',
@@ -12,6 +13,10 @@ export const NODES: Array<string> = [
 	'wk-cdn-f5',
 	'wk-cdn-f6',
 	'wk-cdn-r1',
+	'wk-cdn-r11',
+	'wk-cdn-r12',
+	'wk-cdn-r13',
+	'wk-cdn-r14',
 	'wk-cdn-r2',
 	'wk-cdn-r3',
 	'wk-cdn-r4',
@@ -20,7 +25,11 @@ export const NODES: Array<string> = [
 	'wk-cdn-r7',
 	'wk-cdn-r8',
 	'wk-cdn-r9',
-	'wk-cdn-s1',
+	'wk-cdn-s10',
+	'wk-cdn-s11',
+	'wk-cdn-s12',
+	'wk-cdn-s13',
+	'wk-cdn-s14',
 	'wk-cdn-s2',
 	'wk-cdn-s3',
 	'wk-cdn-s5',
