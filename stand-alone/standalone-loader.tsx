461fc81be4d8874265c1c5ee260104c99e070759
pawelforfa <pforfa@fandom.com>
2023-02-07 17:31:16 -0500

    DPAD-1485 :: Build a separate RedVentureVideoPlayer file in the main dist directory, so that the stand-alone video loader can import just the RedVentureVideoPlayer file, instead of importing all of the files that are found in the compiled main.js file; this was causing the standalone video player bundle to be 100kb larger than it should've been

--- stand-alone/standalone-loader.tsx
+++ stand-alone/standalone-loader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // @ts-ignore (This package does exist, after the fandom player is built with 'yarn build-fandom-player')
-import { RedVentureVideoPlayer } from '@fandom/jwplayer-fandom';
+import RedVentureVideoPlayer from '@fandom/jwplayer-fandom/RedVentureVideoPlayer';
 import {
 	buildRedVentureVideoDetails,
 	canPlayerRender,
@@ -18,21 +18,6 @@ interface WindowWithRedVentureJWPlayer extends Window {
 }
 
 declare let window: WindowWithRedVentureJWPlayer;
-
-// TODO: Investigate the following
-/*
- *
- * -Figure out if the videos should have an item link to the video-platform. Given that
- * the PlayerOne videos might be hosted in a different Workspace on the JW Platform, they won't be available
- * on the video-platform.
- *
- * -Figure out mediaQuery breakpoints between desktop vs mobile on all the sites.
- * Do all the sites have the same exact breakpoints?
- * Is there a table version of any of the sites?
- * Should the sites provide a function that can be used by the player to determine whether
- * it should display a mobile/desktop typo of player?
- *
- * */
 
 let jwPlayerContainerIdIncrementCounter = 0;
 
