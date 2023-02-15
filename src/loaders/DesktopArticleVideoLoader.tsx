5d0eb261142a0836beb03324d72553a03f6be133
pawelforfa <pforfa@fandom.com>
2023-02-15 14:28:22 -0500

    DPAD-1089 VIDEO-229 :: Remove desktop reskin experiment

--- src/loaders/DesktopArticleVideoLoader.tsx
+++ src/loaders/DesktopArticleVideoLoader.tsx
@@ -1,20 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { DesktopArticleVideoLoaderProps } from 'loaders/types';
 import { setVersionWindowVar } from 'loaders/utils/GetVersion';
-import defineExperiment from '@fandom/pathfinder-lite/experiments/defineExperiment';
-import getExperiment from '@fandom/pathfinder-lite/experiments/getExperiment';
-import { Experiment } from '@fandom/pathfinder-lite/types';
 import { shouldLoadUcpPlayer } from 'loaders/utils/shouldLoadPlayer';
 import { eligibleForYoutubeTakeover, getYoutubeTakeoverDetails } from 'loaders/utils/GetYoutubeTakeoverDetails';
 
 export { getVideoPlayerVersion } from 'loaders/utils/GetVersion';
-
-const desktopReskinnedExperiment = defineExperiment({
-	name: 'desktop-reskinned-player',
-	buckets: ['p'],
-	startDate: Date.parse('2022-12-05T08:00:00'),
-	endDate: Date.parse('2022-12-14T11:59:00'),
-});
 
 export const DesktopArticleVideoLoader: React.FC<DesktopArticleVideoLoaderProps> = ({ videoDetails }) => {
 	const [player, setPlayer] = useState(undefined);
@@ -28,19 +18,6 @@ export const DesktopArticleVideoLoader: React.FC<DesktopArticleVideoLoaderProps>
 	}, []);
 
 	const getPlayer = async () => {
-		// By default just set the base player
-
-		const currentExperiment: Experiment = getExperiment([desktopReskinnedExperiment]);
-
-		if (currentExperiment?.name === desktopReskinnedExperiment?.name) {
-			currentExperiment.log.info('Loading re-skinned Desktop Article Video Player');
-			import('experimental/players/DesktopReskinnedArticleVideoPlayer/DesktopReskinnedArticleVideoPlayer').then(
-				({ default: JWDesktopReskinnedArticleVideoPlayer }) =>
-					setPlayer(<JWDesktopReskinnedArticleVideoPlayer videoDetails={videoDetails} />),
-			);
-			return;
-		}
-
 		const youtubeTakeoverDetails = await getYoutubeTakeoverDetails({ deviceType: 'desktop' });
 
 		if (eligibleForYoutubeTakeover(youtubeTakeoverDetails)) {
@@ -49,10 +26,8 @@ export const DesktopArticleVideoLoader: React.FC<DesktopArticleVideoLoaderProps>
 				setPlayer(<YoutubeDesktopArticleVideoPlayer youtubeTakeoverDetails={youtubeTakeoverDetails} />),
 			);
 			return;
-		}
-
-		// By default if there is no experiment just set the base player
-		if (!currentExperiment) {
+		} else {
+			// By default if there is no experiment or youtube embed, then just set the base player
 			console.debug('Loading plain Desktop Article Video Player');
 			import('jwplayer/players/DesktopArticleVideoPlayer/DesktopArticleVideoPlayer').then(
 				({ default: JWDesktopArticleVideoPlayer }) =>
