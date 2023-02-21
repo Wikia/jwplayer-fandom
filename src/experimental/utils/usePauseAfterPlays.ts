7fbc34fa45f3bf8b582e80905d799647091c3ef8
Warren Leaverton <wleaverton@fandom.com>
2023-02-21 14:19:41 -0700

    DPAD-1393: fixing play countto include both playlist and recommended videos

--- src/experimental/utils/usePauseAfterPlays.ts
+++ src/experimental/utils/usePauseAfterPlays.ts
@@ -1,14 +1,17 @@
 import { useEffect, useContext } from 'react';
+import useStateRef from 'experimental/utils/useStateRef';
 import { PlayerContext } from 'jwplayer/players/shared/PlayerContext';
-import { PlaylistItemPlayerEventData } from 'jwplayer/types';
 import JWEvents from 'jwplayer/players/shared/JWEvents';
 
 export default function usePauseAfterPlays(pauseAfter: number) {
 	const { player } = useContext(PlayerContext);
+	const [, setTotalPlays, totalPlaysRef] = useStateRef(0);
 
 	useEffect(() => {
-		player?.on(JWEvents.PLAYLIST_ITEM, (event: PlaylistItemPlayerEventData): void => {
-			if (event.index === pauseAfter) {
+		player?.on(JWEvents.PLAYLIST_ITEM, (): void => {
+			setTotalPlays(totalPlaysRef.current + 1);
+
+			if (totalPlaysRef.current === pauseAfter) {
 				player?.pause();
 			}
 		});
