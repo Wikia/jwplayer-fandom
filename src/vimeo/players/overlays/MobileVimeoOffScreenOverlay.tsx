aa8877ae378518d612e94bd41c0e1e9d15796e07
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-06-27 12:10:40 +0200

    VIDEO-612 add vimeo mobile player

---
+++ src/vimeo/players/overlays/MobileVimeoOffScreenOverlay.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import WDSVariables from '@fandom-frontend/design-system/dist/variables.json';
+import CloseButton from 'youtube/players/shared/CloseButton';
+
+const OffScreenOverlayWrapper = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	pointer-events: none;
+	z-index: ${Number(WDSVariables.z7) + 2};
+`;
+
+interface OffScreenOverlayProps {
+	dismiss: () => void;
+	isScrollPlayer: boolean;
+}
+
+const MobileVimeoOffScreenOverlay: React.FC<OffScreenOverlayProps> = ({ dismiss, isScrollPlayer }) => {
+	if (!isScrollPlayer) return null;
+
+	return (
+		<OffScreenOverlayWrapper>
+			<CloseButton deviceType={'mobile'} dismiss={dismiss} />
+		</OffScreenOverlayWrapper>
+	);
+};
+
+export default MobileVimeoOffScreenOverlay;
