d5236c48084870277798a72153d8466b349dbd93
David Iacovone <diacovone@fandom.com>
2023-08-02 15:18:21 +0200

    FCPPT-24 updated OffScreenOverlay.tsx

--- src/jwplayer/players/MobileArticleVideoPlayer/OffScreenOverlay/OffScreenOverlay.tsx
+++ src/jwplayer/players/MobileArticleVideoPlayer/OffScreenOverlay/OffScreenOverlay.tsx
@@ -9,20 +9,45 @@ import clsx from 'clsx';
 import styles from './offScreenOverlay.module.scss';
 
 interface OffScreenOverlayWrapperProps {
-	playing: boolean;
+	dismiss: () => void;
+	isScrollPlayer: boolean;
 }
 
-const OffScreenOverlayWrapper: React.FC<OffScreenOverlayWrapperProps> = ({ playing }) => {
+const OffScreenOverlayWrapper: React.FC<OffScreenOverlayWrapperProps> = ({ dismiss, isScrollPlayer }) => {
+	const playing = usePlaying();
+	const adBreak = useAdBreak();
+
 	const backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));`;
 	const styleOverrides = {
 		...(playing && { backgroundImage }),
 	};
+	const controlbar = document.querySelector<HTMLElement>('.jw-controlbar');
+	let closeButtonOffset;
+
+	if (adBreak) {
+		console.debug('Ad should be playing. Adding 40px offset.');
+		closeButtonOffset = 40;
+	} else {
+		console.debug("Ad should've finished. Setting top offset to 0px.");
+		closeButtonOffset = 0;
+	}
+
+	if (!playing && !adBreak) {
+		if (controlbar) controlbar.style.visibility = 'hidden';
+	} else {
+		if (controlbar) controlbar.style.visibility = 'visible';
+	}
+
+	if (!isScrollPlayer) return null;
 
 	return (
 		<div
 			className={clsx(`article-featured-video__on-scroll-video-wrapper`, styles.offScreenOverlayWrapper)}
 			style={styleOverrides}
-		/>
+		>
+			<CloseButtonStyled dismiss={dismiss} topOffset={closeButtonOffset} iconColor={'#fff'} />
+			<VideoDetails playing={playing} />
+		</div>
 	);
 };
 
@@ -48,34 +73,7 @@ interface OffScreenOverlayProps {
 }
 
 const OffScreenOverlay: React.FC<OffScreenOverlayProps> = ({ dismiss, isScrollPlayer }) => {
-	const playing = usePlaying();
-	const adBreak = useAdBreak();
-
-	const controlbar = document.querySelector<HTMLElement>('.jw-controlbar');
-	let closeButtonOffset = 0;
-
-	if (adBreak) {
-		console.debug('Ad should be playing. Adding 40px offset.');
-		closeButtonOffset = 40;
-	} else {
-		console.debug("Ad should've finished. Setting top offset to 0px.");
-		closeButtonOffset = 0;
-	}
-
-	if (!playing && !adBreak) {
-		if (controlbar) controlbar.style.visibility = 'hidden';
-	} else {
-		if (controlbar) controlbar.style.visibility = 'visible';
-	}
-
-	if (!isScrollPlayer) return null;
-
-	return (
-		<OffScreenOverlayWrapper playing={playing}>
-			<CloseButtonStyled dismiss={dismiss} topOffset={closeButtonOffset} iconColor={'#fff'} />
-			<VideoDetails playing={playing} />
-		</OffScreenOverlayWrapper>
-	);
+	return <OffScreenOverlayWrapper dismiss={dismiss} isScrollPlayer={isScrollPlayer} />;
 };
 
 export default OffScreenOverlay;
