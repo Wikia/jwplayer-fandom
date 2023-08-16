b5be72dc93cb886f9ebe897bea76e40bce77803d
szymonswiergosz <szymon.swiergosz@gmail.com>
2023-08-16 15:08:36 +0200

    COTECH-683 (fix): handle missing duration property

--- src/jwplayer/utils/formatTime.ts
+++ src/jwplayer/utils/formatTime.ts
@@ -1,15 +1,19 @@
+function isNumber(value: unknown): boolean {
+	return typeof value === 'number' && !isNaN(value);
+}
+
 export function formatTime(duration: number): string {
+	// If duration is below 0 seconds or is not a number it means corrupted data, we don't want to display it
+	// Also return early for 0 seconds
+	if (duration <= 0 || !isNumber(duration)) {
+		return '00:00';
+	}
+
 	const hours = Math.floor(duration / 3600);
 	const minutes = Math.floor((duration - hours * 3600) / 60);
 	const seconds = Math.floor(duration - hours * 3600 - minutes * 60);
 
 	let durationStr = '';
-
-	// If duration is below 0 seconds it means corrupted data, we don't want to display it
-	// Also return early for 0 seconds
-	if (duration <= 0) {
-		return '00:00';
-	}
 
 	if (hours > 0) {
 		durationStr += `${hours < 10 ? '0' : ''}${hours}:`;
