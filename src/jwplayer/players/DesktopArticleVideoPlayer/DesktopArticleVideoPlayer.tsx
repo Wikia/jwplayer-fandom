c7bf95918943a6a6df2fcb7aa1191de4f748e750
Karol Tatala <ktatala@MacBook-Pro-2.local>
2023-10-04 15:37:21 +0200

    COTECH-713 | On scroll player experiment

--- src/jwplayer/players/DesktopArticleVideoPlayer/DesktopArticleVideoPlayer.tsx
+++ src/jwplayer/players/DesktopArticleVideoPlayer/DesktopArticleVideoPlayer.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react';
+import clsx from 'clsx';
 import UnmuteButton from 'jwplayer/players/DesktopArticleVideoPlayer/UnmuteButton';
 import JwPlayerWrapper from 'jwplayer/players/shared/JwPlayerWrapper';
 import { StrategyRulesWrapper } from 'jwplayer/players/shared/StrategyRulesWrapper';
@@ -13,6 +14,16 @@ import { VideoPlaceholder } from 'jwplayer/players/shared/VideoPlaceholder/Video
 import { getArticleVideoConfig } from 'jwplayer/utils/articleVideo/articleVideoConfig';
 import articlePlayerOnReady from 'jwplayer/utils/articleVideo/articlePlayerOnReady';
 import { getDismissedFn } from 'jwplayer/utils/utils';
+import VideoDetails from 'jwplayer/players/DesktopArticleVideoPlayer/VideoDetails';
+import {
+	ON_SCROLL_CUSTOM_UX_WITH_TITLE_BAR_VARIANT_ID,
+	ON_SCROLL_EXPERIMENT_ID,
+	ON_SCROLL_JWPLAYER_UX,
+	ON_SCROLL_LARGER_CUSTOM_UX_WITH_TITLE_BAR_VARIANT_ID,
+	ON_SCROLL_LARGER_CUSTOM_UX_WITHOUT_TITLE_BAR_VARIANT_ID,
+	PLAYER_WITH_ON_SCROLL_ENABLED_URL,
+} from 'utils/experiments/onScrollExperiment';
+import { isOptimizelyExperimentActive, isOptimizelyVariationActive } from 'utils/optimizely';
 
 import styles from './DesktopArticleVideoPlayer.module.scss';
 
@@ -22,14 +33,30 @@ export const DesktopArticleVideoPlayerContent: React.FC<DesktopArticleVideoPlaye
 	const jwpAdsSetupComplete = useJwpAdsSetupComplete();
 	const onScreen = useOnScreen(placeholderRef, '0px', 0.5);
 	const [dismissed, setDismissed] = useState(false);
-	const isScrollPlayer = !(dismissed || onScreen);
+	const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+	const shouldUsePlayerWithOnScroll = isOptimizelyVariationActive(ON_SCROLL_EXPERIMENT_ID, ON_SCROLL_JWPLAYER_UX); // 400x225 player
+	const isScrollPlayer = !(dismissed || onScreen) && isPlayerReady && !shouldUsePlayerWithOnScroll;
+	const shouldRenderVideoDetails =
+		isScrollPlayer &&
+		(!isOptimizelyExperimentActive(ON_SCROLL_EXPERIMENT_ID) ||
+			isOptimizelyVariationActive(ON_SCROLL_EXPERIMENT_ID, [
+				ON_SCROLL_CUSTOM_UX_WITH_TITLE_BAR_VARIANT_ID,
+				ON_SCROLL_LARGER_CUSTOM_UX_WITH_TITLE_BAR_VARIANT_ID,
+			]));
+	const shouldRenderWideOnScrollPlayer =
+		isScrollPlayer &&
+		isOptimizelyVariationActive(ON_SCROLL_EXPERIMENT_ID, [
+			ON_SCROLL_LARGER_CUSTOM_UX_WITH_TITLE_BAR_VARIANT_ID,
+			ON_SCROLL_LARGER_CUSTOM_UX_WITHOUT_TITLE_BAR_VARIANT_ID,
+		]);
+
 	const controlbar = document.querySelector<HTMLElement>('.jw-controlbar');
 	const shareIcon = document.querySelector<HTMLElement>('.jw-controlbar .jw-button-container .jw-settings-sharing');
 	const moreVideosIcon = document.querySelector<HTMLElement>('.jw-controlbar .jw-button-container .jw-related-btn');
 	const pipIcon = document.querySelector<HTMLElement>('.jw-controlbar .jw-button-container .jw-icon-pip');
-	const inputName = 'isDismissed';
-	const [isPlayerReady, setIsPlayerReady] = useState(false);
 
+	const inputName = 'isDismissed';
 	const getDismissed = getDismissedFn(inputName);
 
 	const onPlayerInstanceReady = (playerInstance) => {
@@ -37,7 +64,7 @@ export const DesktopArticleVideoPlayerContent: React.FC<DesktopArticleVideoPlaye
 		setIsPlayerReady(true);
 	};
 
-	if (onScreen) {
+	if (isScrollPlayer) {
 		if (controlbar) controlbar.style.background = 'rgba(0, 0, 0, 0.5)';
 		if (shareIcon) shareIcon.style.display = 'flex';
 		if (moreVideosIcon) moreVideosIcon.style.display = 'flex';
@@ -54,9 +81,11 @@ export const DesktopArticleVideoPlayerContent: React.FC<DesktopArticleVideoPlaye
 			<div className={styles.desktopArticleVideoTopPlaceholder} ref={placeholderRef}>
 				{
 					<div
-						className={
-							isScrollPlayer ? styles.desktopArticleVideoWrapperScrollPlayer : styles.desktopArticleVideoWrapper
-						}
+						className={clsx({
+							[styles.desktopArticleVideoWrapper]: !isScrollPlayer,
+							[styles.desktopArticleVideoWrapperScrollPlayer]: isScrollPlayer,
+							[styles.isWideVariant]: shouldRenderWideOnScrollPlayer,
+						})}
 					>
 						{jwpAdsSetupComplete.strategyRulesEnabled ? (
 							<div>
@@ -91,7 +120,9 @@ export const DesktopArticleVideoPlayerContent: React.FC<DesktopArticleVideoPlaye
 										config={getArticleVideoConfig(videoDetails)}
 										onReady={onPlayerInstanceReady}
 										stopAutoAdvanceOnExitViewport={false}
+										playerUrl={shouldUsePlayerWithOnScroll ? PLAYER_WITH_ON_SCROLL_ENABLED_URL : undefined}
 									/>
+									{shouldRenderVideoDetails && <VideoDetails />}
 									<input type="hidden" value={String(dismissed)} name={inputName} />
 								</div>
 							)
--- src/loaders/DesktopArticleVideoLoader.tsx
+++ src/loaders/DesktopArticleVideoLoader.tsx
@@ -4,40 +4,8 @@ import { setVersionWindowVar } from 'loaders/utils/GetVersion';
 import { shouldLoadUcpPlayer } from 'loaders/utils/shouldLoadPlayer';
 import { eligibleForYoutubeTakeover, getYoutubeTakeoverDetails } from 'loaders/utils/GetYoutubeTakeoverDetails';
 import { eligibleForVimeoTakeover, getVimeoTakeoverDetails } from 'loaders/utils/GetVimeoTakeoverDetails';
-import defineExperiment from '@fandom/pathfinder-lite/experiments/defineExperiment';
-import getExperiment from '@fandom/pathfinder-lite/experiments/getExperiment';
-import { Experiment } from '@fandom/pathfinder-lite/types';
-import checkUserGeo from 'utils/experiments/checkUserGeo';
 
 export { getVideoPlayerVersion } from 'loaders/utils/GetVersion';
-
-const desktopPauseAfterThreePlaysExperiment = defineExperiment({
-	name: 'desktop-pause-after-three-plays-player',
-	buckets: ['q'],
-	startDate: Date.parse('2023-03-02T08:00:00'),
-	endDate: Date.parse('2023-03-10T11:59:00'),
-});
-
-const desktopPauseAfterFivePlaysExperiment = defineExperiment({
-	name: 'desktop-pause-after-five-plays-player',
-	buckets: ['r'],
-	startDate: Date.parse('2023-03-02T08:00:00'),
-	endDate: Date.parse('2023-03-10T11:59:00'),
-});
-
-const desktopPauseAfterTenPlaysExperiment = defineExperiment({
-	name: 'desktop-pause-after-ten-plays-player',
-	buckets: ['s'],
-	startDate: Date.parse('2023-03-02T08:00:00'),
-	endDate: Date.parse('2023-03-10T11:59:00'),
-});
-
-const desktopJwFloatOnScrollExperiment = defineExperiment({
-	name: 'desktop-jw-float-on-scroll-experiment',
-	buckets: ['u'],
-	startDate: Date.parse('2023-03-13T06:00:00'),
-	endDate: Date.parse('2023-03-19T11:59:00'),
-});
 
 export const DesktopArticleVideoLoader: React.FC<DesktopArticleVideoLoaderProps> = ({ videoDetails }) => {
 	const [player, setPlayer] = useState(undefined);
@@ -51,13 +19,6 @@ export const DesktopArticleVideoLoader: React.FC<DesktopArticleVideoLoaderProps>
 	}, []);
 
 	const getPlayer = async () => {
-		const currentExperiment: Experiment = getExperiment([
-			desktopPauseAfterThreePlaysExperiment,
-			desktopPauseAfterFivePlaysExperiment,
-			desktopPauseAfterTenPlaysExperiment,
-			desktopJwFloatOnScrollExperiment,
-		]);
-
 		const youtubeTakeoverDetails = await getYoutubeTakeoverDetails({ deviceType: 'desktop' });
 		const vimeoTakeoverDetails = await getVimeoTakeoverDetails();
 
@@ -73,11 +34,6 @@ export const DesktopArticleVideoLoader: React.FC<DesktopArticleVideoLoaderProps>
 				setPlayer(<VimeoDesktopArticleVideoPlayer vimeoDetails={vimeoTakeoverDetails} />),
 			);
 			return;
-		} else if (currentExperiment?.name === desktopJwFloatOnScrollExperiment?.name && checkUserGeo(['us'])) {
-			import('experimental/players/DesktopFloatOnScrollArticleVideoPlayer/DesktopFloatOnScrollArticleVideoPlayer').then(
-				({ default: DesktopFloatOnScrollArticleVideoPlayer }) =>
-					setPlayer(<DesktopFloatOnScrollArticleVideoPlayer videoDetails={videoDetails} />),
-			);
 		} else {
 			console.debug('Loading default Desktop Article Video Player');
 			import('jwplayer/players/DesktopArticleVideoPlayer/DesktopArticleVideoPlayer').then(
